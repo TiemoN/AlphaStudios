4fa2150cce6db2df1dc2e6d3263805181c00619c
Talib <talib.akinci@gmail.com>
2019-11-28 16:10:41 +0100

    updated MainMenu

---
+++ Tank Tactics/Assets/MenuHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuHandler : MonoBehaviour
+{
+    public GameObject Modus, HowToPlay, Options, Credits;
+    private void Start()
+    {
+        Modus.SetActive(true);
+    }
+    public void ModusMenu()
+    {
+        Modus.SetActive(true);
+        HowToPlay.SetActive(false);
+        Options.SetActive(false);
+        Credits.SetActive(false);
+    }
+    public void HowToPlayMenu()
+    {
+        Modus.SetActive(false);
+        HowToPlay.SetActive(true);
+        Options.SetActive(false);
+        Credits.SetActive(false);
+    }
+    public void OptionsMenu()
+    {
+        Modus.SetActive(false);
+        HowToPlay.SetActive(false);
+        Options.SetActive(true);
+        Credits.SetActive(false);
+    }
+    public void CreditsMenu()
+    {
+        Modus.SetActive(false);
+        HowToPlay.SetActive(false);
+        Options.SetActive(false);
+        Credits.SetActive(true);
+    }
+}
