10366ff9baff7eb976aaad4eb4c6418b55e9c182
Kilian <killi.zocker@gmail.com>
2019-12-09 10:49:17 +0100

    Started work on m´Mine

---
+++ Tank Tactics/Assets/P1Mine.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P1Mine : MonoBehaviour
+{
+    public int despawntime;
+    void Start()
+    {
+        
+    }
+    IEnumerator Sespawn()
+    {
+        yield return new WaitForSeconds(despawntime);
+        Destroy(this.gameObject);
+    }
+}
