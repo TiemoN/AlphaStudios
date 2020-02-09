e5472ad4042163fb3942f20c34e409a708e75a9c
Talib47 <talib.akinci@gmail.com>
2020-02-09 19:02:18 +0100

    Worked on MainMenu

--- LearnProject5/Assets/Scripts/UI/MenuHandler.cs
+++ LearnProject5/Assets/Scripts/UI/MenuHandler.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class MenuHandler : MonoBehaviour
 {
     public AudioSource ScrollingSound;
-    public GameObject Modus, HowToPlay, Options, Credits;
+
+    public GameObject Modus, HowToPlay, Options, Credits, Menu, Background;
+
     private void Start()
     {
         Modus.SetActive(true);
@@ -14,34 +16,42 @@ public class MenuHandler : MonoBehaviour
     public void ModusMenu()
     {
         ScrollingSound.Play();
+        Menu.SetActive(true);
         Modus.SetActive(true); 
         HowToPlay.SetActive(false);
         Options.SetActive(false);
         Credits.SetActive(false);
+        Background.SetActive(true);
     }
    
     public void HowToPlayMenu()
     {
         ScrollingSound.Play();
+        Menu.SetActive(false);
         Modus.SetActive(false);
         HowToPlay.SetActive(true);
         Options.SetActive(false);
         Credits.SetActive(false);
+        Background.SetActive(false);
     }
     public void OptionsMenu()
     {
         ScrollingSound.Play();
+        Menu.SetActive(true);
         Modus.SetActive(false);
         HowToPlay.SetActive(false);
         Options.SetActive(true);
         Credits.SetActive(false);
+        Background.SetActive(true);
     }
     public void CreditsMenu()
     {
         ScrollingSound.Play();
+        Menu.SetActive(true);
         Modus.SetActive(false);
         HowToPlay.SetActive(false);
         Options.SetActive(false);
         Credits.SetActive(true);
+        Background.SetActive(true);
     }
 }
