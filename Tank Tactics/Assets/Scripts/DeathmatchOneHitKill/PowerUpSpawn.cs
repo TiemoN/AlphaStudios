551903f900c42c073e58a8acd9b6dbae2544e779
Talib <talib.akinci@gmail.com>
2020-02-13 07:49:34 +0100

    Fertig

--- Tank Tactics/Assets/Scripts/DeathmatchOneHitKill/PowerUpSpawn.cs
+++ Tank Tactics/Assets/Scripts/DeathmatchOneHitKill/PowerUpSpawn.cs
@@ -45,7 +45,6 @@ public class PowerUpSpawn : MonoBehaviour
                 Destroy(children.gameObject);
             }
         }
-        Destroy(powerUpSpawn);
     }
 
     public void SpawnPowerUp()
